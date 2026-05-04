46a8b423bdf174c74acd3ceb28eeb2a8faa57c4d
alraigah <alraigah88@gmail.com>
2026-05-04 23:31:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './router';
 import { I18nextProvider } from 'react-i18next';
@@ -7,14 +6,10 @@ import { AuthProvider } from './contexts/AuthContext';
 import { useLayoutEffect } from 'react';
 
 function App() {
-  // Force scroll to top IMMEDIATELY before any render
   useLayoutEffect(() => {
-    // Disable browser scroll restoration
     if ('scrollRestoration' in window.history) {
       window.history.scrollRestoration = 'manual';
     }
-    
-    // Force scroll to top
     window.scrollTo(0, 0);
     document.documentElement.scrollTop = 0;
     document.body.scrollTop = 0;
@@ -23,12 +18,11 @@ function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <AuthProvider>
-        <BrowserRouter basename={__BASE_PATH__}>
+        <BrowserRouter>
           <AppRoutes />
         </BrowserRouter>
       </AuthProvider>
     </I18nextProvider>
   );
 }
-
 export default App;
